a80836393b4771556ebef184db5a2215b942f933
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-04-03 16:21:57 -0400

    Criando estrutura de pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './movies/pages/home/home.component';
+import { NowPlayingComponent } from './movies/pages/now-playing/now-playing.component';
+import { PopularComponent } from './movies/pages/popular/popular.component';
+import { UpcomingComponent } from './movies/pages/upcoming/upcoming.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NowPlayingComponent,
+    PopularComponent,
+    UpcomingComponent
   ],
   imports: [
     BrowserModule
